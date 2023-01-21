42985ed63298587692af3b780997057c4e3f795e
Gabriel Oliveira <gabriel.oliveira@escoladnc.com.br>
2023-01-21 19:31:40 -0300

    creating a country quiz with restcountries api

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # pdi-country-quiz
-A country quiz using the restcountries api built in vue js
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">    
+  </div>
+  <div>
+      <select
+      v-model="countrySelect"
+      @change="getCountryFlag()"
+    >
+      <option v-for="country in listCountries" :key="country.name" :value="country.name" :test="country.name">{{ country.name }}</option>
+    </select>
+  </div>  
+<br>
+  <div >
+      <img v-if="descriptionCountry" alt="Country Flag" :src="'' + descriptionCountry[0].flags.png"/>
+  </div>
+  {{ listCountries }}
+  
+  
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      descriptionCountry: "",
+      countrySelect: "",
+      listCountries: [],
+    }
+  },
+
+  created(){
+    this.getCountryNames();
+  },
+
+  methods: {
+
+   async getCountryNames(){
+      const response = await axios.get("https://restcountries.com/v3.1/all")
+             
+      this.listCountries = response.data.map((country) => {
+          const countryDetail ={
+            name: country.name.common,
+            capital: country.capital,
+            flags: country.flags.png,
+          }
+          return countryDetail
+        });
+    },
+
+  async getCountryFlag(){
+    const response = await axios.get(`https://restcountries.com/v3.1/name/${this.countrySelect}`)
+    this.descriptionCountry = response.data
+  }
+  }
+
+}
+</script>
